4e339f4d74b9e1472b269579fdf3bae2b1592fdc
Andrew Watkins <andrew.watkinz@gmail.com>
2014-11-18 18:18:47 -0700

    adds user signup form page

--- db/migrate/20141118205936_create_users.rb
+++ db/migrate/20141118205936_create_users.rb
@@ -2,7 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email
-      t.string :password_digest
+      t.string :encrypted_password
+      t.string :salt
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118210020) do
+ActiveRecord::Schema.define(version: 20141118232641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,8 @@ ActiveRecord::Schema.define(version: 20141118210020) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password_digest"
+    t.string   "encrypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
