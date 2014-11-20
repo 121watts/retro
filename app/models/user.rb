4fdc95622632dbe6dcff6ba0e96c51b0b73afe58
Andrew Watkins <andrew.watkinz@gmail.com>
2014-11-20 13:43:33 -0700

    all tests green

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
   has_many :feels
 
   EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
   validates :email, format: { with: EMAIL_FORMAT, on: :update } 
-  validates_uniqueness_of :uid
+  validates_uniqueness_of :uid, :email
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
     email "jeff@jumpstartlab.com"
-    password "password"
+    
   end
 end
