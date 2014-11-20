3d4b9777a48cb24281f89c021f6564a42044e8d7
Andrew Watkins <andrew.watkinz@gmail.com>
2014-11-20 13:46:27 -0700

    removes user new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  resources :feels, only: [:index]
+  resources :users, except: :index
+  resources :feels, only: [:index, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'signin'  => 'sessions#new'
