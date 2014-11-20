947ceaf82e60b735775db8aa461a24a5fb734136
ericfransen <fransenep@gmail.com>
2014-11-20 11:49:18 -0700

    fixes 1 of 3 tests, skips 2 tests since were building github OAuth and probably dropping self implemented auth

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe 'signing up' do
 
   context 'before creating an account' do
-    it 'comes to the right page' do 
+    it 'comes to the right page' do
       visit root_path
-      expect(page).to have_content 'Sign Up'
+      expect(page).to have_css 'div.intro_text'
     end
 
     it 'can create an account' do
@@ -17,7 +17,7 @@ describe 'signing up' do
       expect(User.all.count).to eq 1
     end
 
-    it 'gets redirected to retro index' do 
+    it 'gets redirected to retro index' do
       visit root_path
       fill_in('user[email]', with: "joe@example.com")
       fill_in('user[password]', with: 'password')
