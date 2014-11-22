55f3936a7c10868e9c47425b44d5495a14da36aa
ericfransen <fransenep@gmail.com>
2014-11-22 10:55:41 -0700

    commit to merge with master

--- app/controllers/feels_controller.rb
+++ app/controllers/feels_controller.rb
@@ -1,4 +1,22 @@
 class FeelsController < ApplicationController
   def index
+    @feel = Feel.new
+  end
+
+  def create
+    @feel = current_user.feels.new(feel_params)
+    if @feel.save
+      flash[:success] = "Your retro has been saved."
+      redirect_to feels_path
+    else
+      flash[:error] = "Something went wrong. Please try again."
+      redirect_to feels_path
+    end
+  end
+
+  private
+
+  def feel_params
+    params.require(:feel).permit(:body, :emoji, :user_id)
   end
 end
