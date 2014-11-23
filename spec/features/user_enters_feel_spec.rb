42e41135deb2ff5b0e0266228089f3f00786bea3
Andrew Watkins <andrew.watkinz@gmail.com>
2014-11-23 15:21:30 -0700

    fixed user entering feels test by renaming button and fixing the phone parsing regex

--- spec/features/user_enters_feel_spec.rb
+++ spec/features/user_enters_feel_spec.rb
@@ -14,26 +14,26 @@ end
 
 it 'can save a feel' do
   fill_in('feel[body]', with: "yesterday all my troubles seemed so far away")
-  click_button 'Create Feel'
+  click_button 'Feel'
   expect(page).to have_content('yesterday all my troubles seemed so far away')
 end
 
 it 'doesn\'t save if blank' do
   #doesn't fill anything in
   Time.new
-  click_button 'Create Feel'
+  click_button 'Feel'
   expect(Feel.all).to eq []
 end
 
 it 'gets a flash message if feel is saved' do
   fill_in('feel[body]', with: "yesterday all my troubles seemed so far away")
-  click_button 'Create Feel'
+  click_button 'Feel'
   expect(page).to have_content('Your retro has been saved.')
 end
 
 it 'gets a flash message if feel is NOT saved' do
   #doesn't fill anything in
-  click_button 'Create Feel'
+  click_button 'Feel'
   expect(page).to have_content('Something went wrong. Please try again.')
   end
 
