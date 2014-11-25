20795fade6a93fd398e9e36e96057621a972e871
Andy Mention <amention@gmail.com>
2014-11-25 15:48:00 -0700

    fix spec to match word case

--- spec/features/returning_user_spec.rb
+++ spec/features/returning_user_spec.rb
@@ -12,8 +12,8 @@ describe 'returning user' do
   end
 
   it 'should be able to logout' do
-    click_on 'Sign Out'
-    expect(page).to_not have_content 'Sign Out'
+    click_on 'sign out'
+    expect(page).to_not have_content 'sign out'
     expect(current_path).to eq root_path
   end
 end
