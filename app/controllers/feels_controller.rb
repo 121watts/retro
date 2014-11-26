4becdd9aee0d7b23ad03a705c162e9d0860435c6
ericfransen <fransenep@gmail.com>
2014-11-25 20:18:15 -0700

    fix failing tests, need to decide if user can write one or many retros per day

--- app/controllers/feels_controller.rb
+++ app/controllers/feels_controller.rb
@@ -2,7 +2,10 @@ class FeelsController < ApplicationController
   before_action :require_login
 
   def index
+    @user = current_user
     @feel = Feel.new
+    @feels = current_user.feels.all
+    @feel_for_today = current_user.feel_for_today
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+      
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
