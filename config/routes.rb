3b8a51b50b16b419a2404b8f07621763cf286e9b
Andy Mention <amention@gmail.com>
2014-12-04 09:12:19 -0700

    Add griddler email endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get '/how' => 'pages#how'
 
+  mount_griddler
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
 end
