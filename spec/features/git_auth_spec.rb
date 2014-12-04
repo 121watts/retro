e6b20c28e20111895a32b7c38a04416b28708ef0
Andy Mention <amention@gmail.com>
2014-12-04 09:06:01 -0700

    Fix specs to work with updated routes

--- spec/features/git_auth_spec.rb
+++ spec/features/git_auth_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'signing into the service' do
   context 'as a new user' do
     it 'can sign in with github' do
-      visit signin_path
+      visit root_path
       expect(page).to have_content "Sign in with GitHub"
       click_on("Sign in with GitHub")
       
@@ -13,7 +13,7 @@ describe 'signing into the service' do
 
     it 'gets redirected to sign in page if auth fails' do
       OmniAuth.config.mock_auth[:github] = :invalid_credentials
-      visit signin_path
+      visit root_path
       expect(page).to have_content "Sign in with GitHub"
       click_on("Sign in with GitHub")
       expect(current_path).to eq root_path
